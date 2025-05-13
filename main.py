963d2ec5febbc1bb8c93442451c701506f2b67f0
teodor933 <teodorancuta1@gmail.com>
2025-05-13 14:55:31 +0100

    Update main.py with a subjectively neater cell ID tracker
    Changed the global ID tracker variable to a factory class that tracks the next ID for each instance.

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import pygame
 import numpy as np
 
 
-cell_id_tracker = 0
-
-
 class Rule:
     def __init__(self, cell, push_strength):
         self.cell = cell
@@ -12,15 +9,21 @@ class Rule:
 
 
 class Cell:
-    def __init__(self, colour: tuple = (255, 255, 255), size: int = 1, rules: list = []):
-        global cell_id_tracker
-
-        self.id = cell_id_tracker
+    def __init__(self, cell_id, colour: tuple = (255, 255, 255), size: int = 1, rules: list = []):
+        self.id = cell_id
         self.colour = colour
         self.size = size
         self.rules = rules
 
-        cell_id_tracker += 1
+
+class CellFactory:
+    def __init__(self):
+        self.next_id = 0
+
+    def create_cell(self, colour: tuple = (255, 255, 255), size: int = 1, rules: list = []):
+        cell = Cell(self.next_id, colour, size, rules)
+        self.next_id += 1
+        return cell
 
 
 def main():
@@ -29,7 +32,8 @@ def main():
     screen = pygame.display.set_mode((600, 600))
     pygame.display.set_caption("Cellular Automata")
 
-    red_cell = Cell(colour=(255, 0, 0), size=5, rules=[
+    factory = CellFactory()
+    red_cell = factory.create(colour=(255, 0, 0), size=5, rules=[
         Rule(0, 1)
     ])
 
