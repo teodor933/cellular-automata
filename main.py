4ad4af2315a6921acd78c2b4d1dbf05b5593c5f1
Lemmmmy <will.er.bradley@gmail.com>
2025-05-13 14:07:40 +0100

    HUUUUUUGE UPDATE JUST DROPPED

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+import numpy
+
+class cell():
+    def __init__(self, colour:tuple=(255, 255, 255), size:int=1, rules:list=[]):
+        self.colour = colour
+        self.size = size
+        self.rules = rules
+
+if __name__ == '__main__':
+    
+    pygame.init()
+
+    screen = pygame.display.set_mode((600, 600))
+    pygame.display.set_caption("Cellular Automata")
+    
+    while True:
+        pass
